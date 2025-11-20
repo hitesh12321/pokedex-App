732628290d8a491f7ba3a886f33d0cd5ee4cd057
hitesh12321 <sainihitesh1123@gmail.com>
2025-11-21 02:59:46 +0530

    UI work done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:pokedex/onboard/onboard.dart';
 import 'package:pokedex/pokedex_view.dart';
 import 'package:pokedex/bloc/pokemon_bloc.dart';
 import 'package:pokedex/bloc/pokemon_event_n_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -17,19 +17,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: Theme.of(context).copyWith(primaryColor: Colors.red),
-      home: MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (_) => PokemonBloc()..add(PokemonPageRequest(page: 0)),
-          ),
-          
-        ],
-        child: PokedexView(),
+   
+
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => PokemonBloc()..add(PokemonPageRequest(page: 0)),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: Theme.of(context).copyWith(
+          primaryColor: Colors.red,
+    
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        ),
+        home: const PokedexOnboardPage(),
       ),
     );
   }
